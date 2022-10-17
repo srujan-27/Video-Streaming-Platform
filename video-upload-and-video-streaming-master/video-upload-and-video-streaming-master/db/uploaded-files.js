731bf6b7bba048d8dd10a93a4f1e86679361b3a0
srujan-27 <76935591+srujan-27@users.noreply.github.com>
2022-10-17 10:28:20 +0530

    Last Commit

---
+++ video-upload-and-video-streaming-master/video-upload-and-video-streaming-master/db/uploaded-files.js
@@ -0,0 +1,10 @@
+{
+    "1665980324378": {
+      "id": "1665980324378",
+      "name": "video.mp4",
+      "size": 52602991,
+      "path": "uploads\\1665980324378-video.mp4",
+      "encoding": "7bit",
+      "details": ""
+    }
+  }
